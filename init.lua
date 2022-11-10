63f1033553ef32c25bbf176d5952d9f0cd1bf8eb
abhijith444 <abhijth8@gmail.com>
2022-11-10 11:00:52 -0600

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,22 @@
+hs.hotkey.bind({"ctrl", "alt"}, "p", function()
+  hs.eventtap.keyStrokes("******")
+  hs.eventtap.keyStroke({},"return")
+end)
+
+hs.hotkey.bind({"ctrl", "alt"}, "u", function()
+  hs.eventtap.keyStrokes("******")
+end)
+
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
